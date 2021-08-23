76b2dbeec869176fe8b2cf97264b97f07c7c8c71
iago <iago.pferreiravr@gmail.com>
2021-08-23 18:55:41 -0300

    Criacao do componente Aside

---
+++ src/components/Aside.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Aside() {
+  return (
+    <aside>
+      <p>Navigation button</p>
+      <p>Home</p>
+      <p>Search</p>
+      <p>Projects</p>
+      <p>About</p>
+
+    </aside>
+  );
+}
+
+export default Aside;
