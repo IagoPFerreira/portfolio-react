05fa7650981ecbc321675a1c16203e117bbc9eec
iago <iago.pferreiravr@gmail.com>
2021-08-23 19:42:00 -0300

    Adicao dos icones do menu de navegacao

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,13 +1,31 @@
+import { faHome, faBars, faAt } from '@fortawesome/free-solid-svg-icons';
+import { faNewspaper, faTimesCircle, faWindowMaximize } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Aside() {
   return (
     <aside>
+      <FontAwesomeIcon icon={faBars} />
+      <FontAwesomeIcon icon={faTimesCircle} />
       <p>Navigation button</p>
-      <p>Home</p>
-      <p>Search</p>
-      <p>Projects</p>
-      <p>About</p>
+      <Link to="/">
+        <FontAwesomeIcon icon={faHome} />
+        <p>Home</p>
+      </Link>
+      <Link to="/sobre">
+        <FontAwesomeIcon icon={faNewspaper} />
+        <p>Sobre</p>
+      </Link>
+      <Link to="/projetos">
+        <FontAwesomeIcon icon={faWindowMaximize} />
+        <p>Projetos</p>
+      </Link>
+      <Link to="/contatos">
+        <FontAwesomeIcon icon={faAt} />
+        <p>Contatos</p>
+      </Link>
 
     </aside>
   );
