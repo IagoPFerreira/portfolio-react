ab8d3430542d83f8a57f8b98cbc8108d0940eacb
iago <iago.pferreiravr@gmail.com>
2021-09-07 11:39:00 -0300

    Create MenuList component

---
+++ src/components/MenuList.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { faHome, /* faBars */ faAt } from '@fortawesome/free-solid-svg-icons';
+import { faNewspaper, /* faTimesCircle */ faWindowMaximize } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
+
+function MenuList() {
+  return (
+    <ul>
+      <li>
+        <Link to="/">
+          <FontAwesomeIcon icon={faHome} />
+          <p>Home</p>
+        </Link>
+      </li>
+      <li>
+        <Link to="/sobre">
+          <FontAwesomeIcon icon={faNewspaper} />
+          <p>Sobre</p>
+        </Link>
+      </li>
+      <li>
+        <Link to="/projetos">
+          <FontAwesomeIcon icon={faWindowMaximize} />
+          <p>Projetos</p>
+        </Link>
+      </li>
+      <li>
+        <Link to="/contatos">
+          <FontAwesomeIcon icon={faAt} />
+          <p>Contatos</p>
+        </Link>
+      </li>
+    </ul>
+  );
+}
+
+export default MenuList;
--- src/components/Navegation.jsx
+++ src/components/Navegation.jsx
@@ -1,10 +1,7 @@
-import { faHome, /* faBars */ faAt } from '@fortawesome/free-solid-svg-icons';
-import { faNewspaper, /* faTimesCircle */ faWindowMaximize } from '@fortawesome/free-regular-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-import { Link } from 'react-router-dom';
 import MenuButton from './MenuButton';
 import '../styles/Navegation.css';
+import MenuList from './MenuList';
 
 function Navegation() {
   return (
@@ -12,24 +9,7 @@ function Navegation() {
       {/* <FontAwesomeIcon icon={faBars} />
       <FontAwesomeIcon icon={faTimesCircle} /> */}
       <MenuButton />
-      <p>Navigation button</p>
-      <Link to="/">
-        <FontAwesomeIcon icon={faHome} />
-        <p>Home</p>
-      </Link>
-      <Link to="/sobre">
-        <FontAwesomeIcon icon={faNewspaper} />
-        <p>Sobre</p>
-      </Link>
-      <Link to="/projetos">
-        <FontAwesomeIcon icon={faWindowMaximize} />
-        <p>Projetos</p>
-      </Link>
-      <Link to="/contatos">
-        <FontAwesomeIcon icon={faAt} />
-        <p>Contatos</p>
-      </Link>
-
+      <MenuList />
     </aside>
   );
 }
