16935b5ce84b3ba4cd43504b82f14e219d0e8d0b
iago <iago.pferreiravr@gmail.com>
2021-10-18 14:50:20 -0300

    Refact: Removed components MenuButtons and MenuList and replace with Links

--- src/components/Navegation.jsx
+++ src/components/Navegation.jsx
@@ -1,25 +1,53 @@
-import React, { useState } from 'react';
-import MenuButton from './MenuButton';
+import React/* ,  { useState } */ from 'react';
+// import MenuButton from './MenuButton';
 import '../styles/Navegation.css';
-import MenuList from './MenuList';
+// import MenuList from './MenuList';
+import { faHome, /* faBars */ faAt } from '@fortawesome/free-solid-svg-icons';
+import { faNewspaper, /* faTimesCircle */ faWindowMaximize } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { Link } from 'react-router-dom';
+import { Link } from 'react-scroll';
 
 function Navegation() {
-  const [display, setDisplay] = useState('none');
+  // const [display, setDisplay] = useState('hide');
 
-  const changeClassList = (e) => {
-    e.classList.toggle('change');
-    if (display === 'none') {
-      setDisplay('block');
-    } else {
-      setDisplay('none');
-    }
-  };
+  // const changeClassList = (e) => {
+  //   e.classList.toggle('change');
+  //   if (display === 'hide') {
+  //     setDisplay('show');
+  //   } else {
+  //     setDisplay('hide');
+  //   }
+  // };
 
   return (
-    <aside>
-      <MenuButton onClick={changeClassList} />
-      <MenuList display={display} />
-    </aside>
+    <nav className="menu">
+      {/* <MenuButton onClick={changeClassList} /> */}
+      {/* <MenuList /> */}
+      <Link
+        activeClass="active"
+        to="main"
+        spy
+        smooth
+        offset={-70}
+        duration={500}
+      >
+        <FontAwesomeIcon icon={faHome} />
+        <p className="link-name">Home</p>
+      </Link>
+      <Link to="/sobre">
+        <FontAwesomeIcon icon={faNewspaper} />
+        <p className="link-name">Sobre</p>
+      </Link>
+      <Link to="/projetos">
+        <FontAwesomeIcon icon={faWindowMaximize} />
+        <p className="link-name">Projetos</p>
+      </Link>
+      <Link to="/contatos">
+        <FontAwesomeIcon icon={faAt} />
+        <p className="link-name">Contatos</p>
+      </Link>
+    </nav>
   );
 }
 
