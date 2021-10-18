a8ef7024dcbca16a653b46c6e896990e22502388
iago <iago.pferreiravr@gmail.com>
2021-10-18 14:48:17 -0300

    Refact: Remove Switcher and replace with Component Home

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter } from 'react-router-dom';
-import Switcher from './components/Switcher';
+// import Switcher from './components/Switcher';
+import Home from './pages/Home';
 
 function App() {
   return (
     // eslint-disable-next-line react/jsx-filename-extension
     <BrowserRouter>
       <section className="App">
-        <Switcher />
+        {/* <Switcher /> */}
+        <Home />
       </section>
     </BrowserRouter>
   );
