978a827bcb9e1e15b6ed203188fe2ee56d6d8a8d
iago <iago.pferreiravr@gmail.com>
2021-10-18 14:54:39 -0300

    Feat: Create component Main

---
+++ src/pages/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <main id="main">
+      <h1>Iago de Paula Ferreira</h1>
+      <h3>Desenvolvedor Júnior</h3>
+    </main>
+  );
+}
+
+export default Main;
