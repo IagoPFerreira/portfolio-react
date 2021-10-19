78f03566c74686aef359f2e4b3c9092f315f62dd
iago <iago.pferreiravr@gmail.com>
2021-10-19 13:48:47 -0300

    Feat: render projects infos

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function ProjectCard() {
+function ProjectCard({ modulo }) {
   return (
-    <section>
-      Card
-    </section>
+    <>
+      {modulo.map(({ name, repo }) => (
+        <section>
+          <h5>{name}</h5>
+          <a href={repo} target="_blank" rel="noreferrer" className="repo-links">Repositório</a>
+        </section>
+      ))}
+    </>
   );
 }
 
 export default ProjectCard;
+
+ProjectCard.propTypes = {
+  modulo: PropTypes.object,
+}.isRequired;
