0f72627b266665138973217b7d21dddd1ae2a2d4
iago <iago.pferreiravr@gmail.com>
2021-10-19 13:46:45 -0300

    Fix: Changed the import routes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Header from '../components/Header';
 import '../styles/Home.css';
-import About from './About';
-import Contact from './Contact';
-import Main from './Main';
-import Projects from './Projects';
+import About from '../sections/About';
+import Contact from '../sections/Contact';
+import Main from '../sections/Main';
+import Projects from '../sections/Projects';
 
 /* <img src="../imagens/foto_Iago_Ferreira.jpeg"
 alt="Iago de óculos escuro, sorrindo, com um um lago e uma montanha atrás dele"></img> */
