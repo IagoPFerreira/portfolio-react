66f95412228e61175f305976c1163393d60a6ea2
iago <iago.pferreiravr@gmail.com>
2021-10-19 13:46:10 -0300

    Refact: Changed all values

--- src/data/projectsList.js
+++ src/data/projectsList.js
@@ -1,176 +1,168 @@
-const projectList = [
-  {
-    Fundamentos: [
-      {
-        name: 'Lessons Learned',
-        repo: 'https://github.com/IagoPFerreira/sd-010-a-project-lessons-learned',
-      },
-      {
-        name: 'Playground Functions',
-        repo: 'https://github.com/IagoPFerreira/sd-010-a-project-playground-functions',
-      },
-      {
-        name: 'Pixel Art',
-        repo: 'https://github.com/IagoPFerreira/sd-010-a-project-pixels-art',
-      },
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-    ],
-  },
-  {
-    'Front-end': [
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-    ],
-  },
-  {
-    'Back-end': [
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-      {
-        name: 'Blogs API',
-        repo: 'https://github.com/IagoPFerreira/sd-010-a-project-blogs-api',
-      },
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-    ],
-  },
-  {
-    'Ciencia da computação': [
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-      {
-        name: 'HTML',
-        repo: 'https://github.com/IagoPFerreira',
-      },
-    ],
-  },
-];
+const projectList = {
+  Fundamentos: [
+    {
+      name: 'Lessons Learned',
+      repo: 'https://github.com/IagoPFerreira/sd-010-a-project-lessons-learned',
+    },
+    {
+      name: 'Playground Functions',
+      repo: 'https://github.com/IagoPFerreira/sd-010-a-project-playground-functions',
+    },
+    {
+      name: 'Pixel Art',
+      repo: 'https://github.com/IagoPFerreira/sd-010-a-project-pixels-art',
+    },
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+  ],
+  'Front-end': [
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+  ],
+  'Back-end': [
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+    {
+      name: 'Blogs API',
+      repo: 'https://github.com/IagoPFerreira/sd-010-a-project-blogs-api',
+    },
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+  ],
+  'Ciencia da computação': [
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+    {
+      name: 'GITHUB',
+      repo: 'https://github.com/IagoPFerreira',
+    },
+  ],
+};
 
 export default projectList;
