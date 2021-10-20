4de44a3172d2e1b4de9a317f076b6cf1a5cd831b
iago <iago.pferreiravr@gmail.com>
2021-10-20 08:50:52 -0300

    Feat:  render contact informations

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,54 +1,22 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 function Contact() {
   return (
     <section id="contact">
-      <span>Contato</span>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-        molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-        numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-        optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-        obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-        nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-        tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-        quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
-        sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-        recusandae alias error harum maxime adipisci amet laborum. Perspiciatis
-        minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit
-        quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur
-        fugiat, temporibus enim commodi iusto libero magni deleniti quod quam
-        consequuntur! Commodi minima excepturi repudiandae velit hic maxime
-        doloremque. Quaerat provident commodi consectetur veniam similique ad
-        earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo
-        fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore
-        suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantium
-        modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam
-        totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam
-        quasi aliquam eligendi, placeat qui corporis!
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-        molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-        numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-        optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-        obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-        nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-        tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-        quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
-        sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-        recusandae alias error harum maxime adipisci amet laborum. Perspiciatis
-        minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit
-        quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur
-        fugiat, temporibus enim commodi iusto libero magni deleniti quod quam
-        consequuntur! Commodi minima excepturi repudiandae velit hic maxime
-        doloremque. Quaerat provident commodi consectetur veniam similique ad
-        earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo
-        fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore
-        suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantium
-        modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam
-        totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam
-        quasi aliquam eligendi, placeat qui corporis!
+      <h2>Contato</h2>
+      <a href="https://github.com/IagoPFerreira" target="_blank" rel="noreferrer">
+        <FontAwesomeIcon icon={faGithub} className="icon-contact" />
+      </a>
+      <a href="https://www.linkedin.com/in/iago-de-paula-ferreira-45b501128/" target="_blank" rel="noreferrer">
+        <FontAwesomeIcon icon={faLinkedin} className="icon-contact" />
+      </a>
+      <p href="mailto:iago.pferreiravr@gmail.com" target="_blank" rel="noreferrer">
+        <FontAwesomeIcon icon={faEnvelope} className="icon-contact" />
+        {' '}
+        iago.pferreiravr@gmail.com
       </p>
     </section>
   );
