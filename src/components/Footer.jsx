6c8c6f26db1bcda7ddf17d0e44ff8b9a5b56fcd7
iago <iago.pferreiravr@gmail.com>
2021-10-26 09:52:06 -0300

    Feat: render all contacts info on component Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
-function Footer(props) {
+function Footer() {
   return (
-    <div>
-      
-    </div>
+    <footer id="contact">
+      <h2>Contato</h2>
+      <section className="contacts-container">
+        <a href="https://github.com/IagoPFerreira" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon icon={faGithub} className="icon-contact" />
+        </a>
+        <a href="https://www.linkedin.com/in/iago-de-paula-ferreira-45b501128/" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon icon={faLinkedin} className="icon-contact" />
+        </a>
+        <p href="mailto:iago.pferreiravr@gmail.com" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon icon={faEnvelope} className="icon-contact" />
+          {' '}
+          iago.pferreiravr@gmail.com
+        </p>
+      </section>
+    </footer>
   );
 }
 
