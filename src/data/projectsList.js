a26043d7c5b64d2d91012bd66575db8ab3b8babf
iago <iago.pferreiravr@gmail.com>
2021-10-30 18:31:34 -0300

    Refact: rewriting projectList.js, adding images paths and commenting project  that are not ready

--- src/data/projectsList.js
+++ src/data/projectsList.js
@@ -2,166 +2,174 @@ const projectList = {
   Fundamentos: [
     {
       name: 'Lessons Learned',
-      repo: 'https://github.com/IagoPFerreira/sd-010-a-project-lessons-learned',
+      image: 'https://github.com/IagoPFerreira/lessons-learned/raw/main/images/lessons-learned.png',
+      alt: 'Demonstrando o inicio do projeto lessons learned, com um cabeçalho com o nome do projeto e links, uma foto de perfil e 2 tabelas com os conhecimentos de Iago',
+      repo: 'https://github.com/IagoPFerreira/lessons-learned',
+      deploy: 'https://iagopferreira.github.io/lessons-learned/',
     },
     {
       name: 'Playground Functions',
-      repo: 'https://github.com/IagoPFerreira/sd-010-a-project-playground-functions',
+      image: 'https://github.com/IagoPFerreira/playground-functions/raw/main/playground-functions.png',
+      alt: 'Demonstrando a execução de uma função do projeto playground-functions',
+      repo: 'https://github.com/IagoPFerreira/playground-functions',
     },
     {
       name: 'Pixel Art',
-      repo: 'https://github.com/IagoPFerreira/sd-010-a-project-pixels-art',
+      repo: 'https://github.com/IagoPFerreira/pixels-art',
+      alt: 'Demonstrando o inicio do projeto pixels art, com uma paleta de cores para escolher, 2 botões e e uma tabela para ser pintada',
+      image: 'https://github.com/IagoPFerreira/pixels-art/raw/main/pixels-art.png',
+      deploy: 'https://iagopferreira.github.io/pixels-art/',
     },
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
   ],
   'Front-end': [
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
   ],
   'Back-end': [
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
-    {
-      name: 'Blogs API',
-      repo: 'https://github.com/IagoPFerreira/sd-010-a-project-blogs-api',
-    },
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
+    // {
+    //   name: 'Blogs API',
+    //   repo: 'https://github.com/IagoPFerreira/sd-010-a-project-blogs-api',
+    // },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
   ],
   'Ciencia da computação': [
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
-    {
-      name: 'GITHUB',
-      repo: 'https://github.com/IagoPFerreira',
-    },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
+    // {
+    //   name: 'GITHUB',
+    //   repo: 'https://github.com/IagoPFerreira',
+    // },
   ],
 };
 
